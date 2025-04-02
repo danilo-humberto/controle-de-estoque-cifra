82e409dc523a707cefd41005f40592d69d2457c1
Danilo Humberto <danilohumberto12@hotmail.com>
2025-04-02 16:29:39 -0300

    chore: adding new requests

--- src/functions/api.js
+++ src/functions/api.js
@@ -2,10 +2,61 @@ import axios from 'axios'
 
 const API_URL = "http://192.168.15.61:5000"
 
+
+// GET ROUTES
 export const getLinhas = async () => {
     try {
-        const response = await axios.get(`${API_URL}/visualizar_linhas`)
+        const response = await axios.get(`${API_URL}/linhas`)
         return response.data;
+    } catch (error) {
+        console.error("Erro ao buscar dados: ", error)
+        throw error;
+    }
+}
+
+export const getFuncionarios = async () => {
+    try {
+        const response = await axios.get(`${API_URL}/funcionarios`)
+        return response.data;
+    } catch (error) {
+        console.error("Erro ao buscar dados: ", error)
+        throw error;
+    }
+}
+
+export const getEquipamentos = async () => {
+    try {
+        const response = await axios.get(`${API_URL}/equipamentos`)
+        return response.data;
+    } catch (error) {
+        console.error("Erro ao buscar dados: ", error)
+        throw error;
+    }
+}
+
+// POST ROUTES
+export const postFuncionarios = async (dados) => {
+    try {
+        const response = await axios.post(`${API_URL}/funcionarios/cadastrar`, dados, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            }
+        })
+        return response.data
+    } catch (error) {
+        console.error("Erro ao buscar dados: ", error)
+        throw error;
+    }
+}
+
+export const postEquipamentos = async (dados) => {
+    try {
+        const response = await axios.post(`${API_URL}/equipamentos/cadastrar`, dados, {
+            headers: {
+                'Content-Type': 'multipart/form-data',
+            }
+        })
+        return response.data
     } catch (error) {
         console.error("Erro ao buscar dados: ", error)
         throw error;
