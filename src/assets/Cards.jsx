0f131c5f405ad08fb66f50ec91c6667cbc8e6f1e
Danilo Humberto <danilohumberto12@hotmail.com>
2025-04-02 13:39:18 -0300

    fix: fixing height

--- src/assets/Cards.jsx
+++ src/assets/Cards.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Cards = ({span, icon, color}) => {
   return (
     <div 
-        className={`${color} w-[200px] h-24 rounded-xl cursor-pointer transition-all duration-300`}
+        className={`${color} w-[200px] 2xl:h-24 xl:h-20 rounded-xl cursor-pointer transition-all duration-300`}
     >
       <div className='flex items-center justify-around h-full text-[var(--gray-300)]'>
         <span>{span}</span>
