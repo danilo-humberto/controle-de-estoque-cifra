c1944282db6f5fbc0db4797b070dec36a5d67931
Danilo Humberto <danilohumberto12@hotmail.com>
2025-04-02 16:30:22 -0300

    chore: adding toast component

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import "./main.css";
 import "./root.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "./components/ui/toaster";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <App />
+      <Toaster />
     </BrowserRouter>
   </StrictMode>
 );
